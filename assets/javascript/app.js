02c754b35ed0d22f11889bb81e77a06b69d38314
Jared Williams <jdwux80@gmail.com>
2019-04-08 14:52:29 -0400

    Light HTML and psuedo coding done in inxdex and app js file.

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// *********** Psuedo **************
+
+// Prompt user to start the game with a start button. 
+
+// After start button is clicked, start the timer countdown and display questions and answer choices.
+
+// Allow user to pick only 1 answer per question.
+
+// Create array of answer choices for each question.
+
+// Store # of correct answers into new array
+
+// Store # of incorrect answers into new array
+
+// Store # of unanswered questions into new array
+
+// End the game and display the number of correct,incorrect, and unanswered questions when...
+    // time runs 
+    // Tthe players has clicked "done"
+
