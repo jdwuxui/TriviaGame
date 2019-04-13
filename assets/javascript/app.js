e00684f27982e3a8d6e9e37e6f50860b6305c233
Jared Williams <jdwux80@gmail.com>
2019-04-12 21:10:44 -0400

    Countdown timer added, but not starting on click event yet.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,152 +10,101 @@ element.appendChild(para);
 
 // Display instructions
 var instructions = document.createElement("h4");
-var instructionText = document.createTextNode("Test your knowledge of the Predator species by answering as many questions as you can before time runs out.");
+var instructionText = document.createTextNode("Test your knowledge of the movie 'Predator' by answering trivia questions before time runs out.");
 instructions.appendChild(instructionText);
 var element = document.getElementById("Instructions-Text");
 element.appendChild(instructions);
 //THIS WORKS^^^
 
-// Display start button
+// Display start button with custom CSS class
 var startButton = document.createElement("button");
 var startButtonText = document.createTextNode("START GAME");
 startButton.appendChild(startButtonText);
 var element = document.getElementById("Start-Button");
 element.appendChild(startButton);
-//THIS WORKS^^^
-//Add a custom class to the button 
+//Add a class to button 
 startButton.classList.add("alien-glow");
 //THIS WORKS^^^
 
+// GLOBAL VARIABLES
+var correctAnswers = 0;
+var WrongAnswers = 0;
+var playerGuess = [];
+var triviaQuestions = 0;
+var triviaQuestions = [{
+    question: "1. What year was the movie 'Predator' released?",
+    answers: ["1984", "1985", "1986", "1987"],
+    correctAnswer: 3
+    },
+    {
+    question: "2. In the first Predator movie, which one-liner does Arnold Schwarzenegger say after impaling an enemy against a wooden post with his machete?",
+    answers: ["Stay put", "Stick around", "Snap into a slim jim", "Stick to it"],
+    correctAnswer: 1
+    },
+    {
+    question: "3. Which martial-arts expert was originally cast to play the Predator?",
+    answers: ["Jackie Chan", "Jean-Claude Van Damme ", "Wesley Snipes", "Steven Seagal"],
+    correctAnswer: 1
+    },
+    {
+    question: "4. What animal did Mac kill with his knife?",
+    answers: ["Scorpion", "Wild Boar", "A Scorpion and Wild Boar", "Monkey"],
+    correctAnswer: 1
+    }, 
+    {
+    question: "5. What ingredients were used to make the predator's blood?",
+    answers: ["Glow sticks and KY Jelly", "Mountain Dew and rubber cement", "Melted crayons", "Food dye and Vasoline"],
+    correctAnswer: 0
+    }
+    ];
 
-// Clicking the start button runs the game 
-//Create a function for the
+// The player clicks the start button to run the game and timer.
+document.getElementById("Start-Button").addEventListener("click", startTheGame);
 
-// Start the timer countdonwn for the question
+// Create event listener for Start Button and name its function
 
-// Show a question 
+// The start button and instructions should be hidden
 
-// Show the answer choices
+// Show only one question until the player answers it or their time runs out.
+    
 
-// Allow the user to skip question
+//start the timer countdonwn after the start button is clicked 
+        ///...and when the player an answer is clicked.
+        var countdown = 10;
+      
+        var timer = setInterval(function() {
+          console.log(countdown);
+          countdown--;
+          if(countdown === 0) {
+            stopInterval()
+          }
+        }, 1000);
+        
+        var stopInterval = function() {
+          console.log("You're too slow!");
+          clearInterval(timer);
+        }
+// Display timer
+ ///...then start the timer
 
-// If answer is skipped then add to Unanswered Questions
+ // Display the first question
+                            
+// Show answer choices
 
-// Let the user choose 1 of 4 answer choices
+// Only allow 1 answer to be selected 
 
-// Click next to go to next question
+// Notify the player if they got the answer right or wrong
+    ///...and show a gif or image associated with it
+
+// If the timer runs out the correct answer is displayed with an gif or image
+    ///...then the time is reset
+        ////...and the next question is dislayed
+
+///...and +1 will be added to Unanswered Questions score 
+// If answer is skipped +1 will be added to Unanswered Questions score 
+// If answer is wrong +1 will be added to Unanswered Questions score 
+// If the answer is correct +1 will be added to Correct Answers score
 
 // Store the selected answer/index value
 
 // Show a new question
-
-
-
-
-// Select div id's
-const triviaConsole = document.getElementById("#game-console");
-const submitButton = document.getElementById("#done");
-const scoresContainer = document.getElementById("#scores");
-
-startTriviaGame(); { 
-    $("#start-button").addEventListener("click", runTriviaGame);
-}
-
-//write a function to start a new game
-$("#start-button").on("click",function(){
-    $("#game-console").show();
-    // call start timer function
-    });
-
-// Create function to run the game
-function runTriviaGame() {
-    // create array of objects for questions with answer choices
-const triviaQuestions = [ 
-    {
-        question: "What year was the movie 'Predator' released?",
-        answers: ["1984", "1985", "1986", "1987"],
- // integer of the index value = corect answer
-        correctAnswer: 3,
-        },
-       
- 
-];
-    // ...store html output
-    const output = [];
-    // 
-    triviaQuestions.forEach( (currentQuestion, questionNumber) => {
-        const answers = [];
-        for(letter in currentQuestion.answers) {
-// ...add radio buttons
-        answers.push(
-            `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
-      // ...add question and answers to output 
-      output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join('')} </div>`);
-        }
-        );
-        // ...combine output list into string 
-        triviaConsole.innerHTML = output.join('');
-    }
-    // ...for each loop through questions 
-    triviaQuestions.forEach( (currentQuestion, questionNumber) => {
-    // ...code to run for questions
-    // Store answer choices 
-    const answers = [];
-    // and for each available answer... 
-    for(letter in currentQuestion.answers){ 
-        // ...add an html radio button 
-        answers.push(
-            `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-            </label>`
-            );
-        }
-        // add Q&A to output
-        output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
-            );
-        });
-
-function displayScores() {
-    // gather answer container data
-    const answerContainers = triviaConsole.querySelectorAll('.answers');
-    // track player guess
-    let numCorrect = 0;
-    // For each question...
-    triviaQuestions.forEach( (currentQuestion, questionNumber) => {
-        // find selected answer
-    const answerContainer = answerContainers[questionNumber];
-    const selector = 'input[name=question'+questionNumber+']:checked';
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-    // if answer is correct
-    if(userAnswer===currentQuestion.correctAnswer){
-        // add to number of correct answers
-        numCorrect++;
-          // show number of correct answers out of total
-  resultsContainer.innerHTML = numCorrect + ' out of ' + triviaQuestions.length;
-}
-
-
-// Jquery toggle CSS display none and display block to hide and show content 
-
-// END OF GAME --- CLICK DONE
-$("#done").on("click", function(event) {
-    event.preventDefault();
-var q1answer = $('input[name=Q1A]:checked').val(); }
-// stop timer function
-
-// END OF GAME --- TIME UP 
-// stop timer 
-// show results 
-)})}
